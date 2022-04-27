b285867ded52c994cc82afbb70b371cce3d1cf96
prajaktahadole <prajaktahadole@gmail.com>
2022-04-27 17:40:42 +0530

    inventory

--- 3 Inventory/app/src/components/Inventory.jsx
+++ 3 Inventory/app/src/components/Inventory.jsx
@@ -1,80 +1,123 @@
 import { useState } from "react";
-import "./Inventory.css"
 
 export const Inventory = () => {
-  const [inv, setInv] = useState({
-    books: 10,
-    notebooks: 13,
-    pens: 40,
-    inkpens : 15
-  });
+  const [bookcount, setbookcount] = useState(10);
+  const [notebookcount, setnotebookcount] = useState(13);
+  const [pencount, setpencount] = useState(40);
+  const [inkpencount, setinkpencount] = useState(10);
+
+  const bookAddHandle = (value) => {
+    if(bookcount >= 0){
+        setbookcount(bookcount + value);
+    } else {
+        setbookcount(bookcount);
+    }
+  }
+
+  const bookSubHandle = (value) => {
+    if(bookcount > 0){
+        setbookcount(bookcount - value);
+    } else {
+        setbookcount(bookcount);
+    }
+  }
+
+  const notebookAddHandle = (value) => {
+      setnotebookcount(notebookcount + value);
+  }
+
+  const notebookSubHandle = (value) => {
+      if(notebookcount > 0){
+        setnotebookcount(notebookcount - value);
+      } else{
+        setnotebookcount(notebookcount)
+      }
+  }
+
+  const penAddHandle = (value) => {
+      setpencount(pencount + value);
+  }
+
+  const penSubHandle = (value) => {
+      if(pencount > 0){
+        setpencount(pencount - value);
+      } else{
+        setpencount(pencount)
+      }
+    
+  }
+
+
+  const inkpenAddHandle = (value) => {
+      setinkpencount(inkpencount + value);
+  }
+
+  const inkpenSubHandle = (value) => {
+    if(inkpencount > 0){
+        setinkpencount(inkpencount - value);
+    }
+    else {
+        setinkpencount(inkpencount);
+    }
+    
+ }
+
+
     // Create add and remove functions here that changes the
     // state.
-    const handlebook = (value) =>{
-
-      if (inv + value < 0){
-         return 
-      }
-      setCount(inv+ value);
-  }
-  const handleNotebook = (value) =>{
-
-    if (inv + value < 0){
-       return 
-    }
-    setCount(inv+ value);
-  }
-
-  const pens = (value) =>{
-
-    if (inv + value < 0){
-       return 
-    }
-    setCount(inv+ value);
-}
-
-const inkpens = (value) =>{
-
-  if (inv+ value < 0){
-     return 
-  }
-  setCount(inv+ value);
-}
-
-
-
-
-
-
-
   return (
-    <div className="istyle">
+    <div
+      style={{
+        border: "1px solid black",
+        borderRadius: "3px",
+        padding: "10px",
+        display: "flex",
+        flexDirection: "column",
+        width: "400px",
+      }}
+    >
       <div className="items">
-        <span>Books:{books} </span>
-        <button onClick={()=> handlebook(1)} className="circlularButton">+</button>
-        <button onClick={()=> handlebook(-1)} className="circlularButton">-</button>
-        <span>{inv.books}</span>
+        <span>Books: </span>
+        <button className="circlularButton" onClick={() => {
+            bookAddHandle(1);
+        }}>+</button>
+        <button className="circlularButton" onClick={() => {
+            bookSubHandle(1);
+        }}>-</button>
+        <span>{bookcount}</span>
       </div>
       <div className="items">
-        <span>Notebooks:{notebooks} </span>
-        <button onClick={()=> handleNotebook(1)} className="circlularButton">+</button>
-        <button onClick={()=> handleNotebook(-1)} className="circlularButton">-</button>
-        <span>{inv.notebooks}</span>
+        <span>Notebooks: </span>
+        <button className="circlularButton" onClick={() => {
+            notebookAddHandle(1);
+        }}>+</button>
+        <button className="circlularButton"onClick={() => {
+            notebookSubHandle(1);
+        }}>-</button>
+        <span>{notebookcount}</span>
       </div>
       <div className="items">
-        <span>Pen:{pens} </span>
-        <button onClick={()=> handlePen(1)} className="circlularButton">+</button>
-        <button onClick={()=> handlePen(-1)} className="circlularButton">-</button>
-        <span>{inv.pens}</span>
+        <span>Pen: </span>
+        <button className="circlularButton" onClick={() => {
+            penAddHandle(1);
+        }}>+</button>
+        <button className="circlularButton" onClick={() => {
+            penSubHandle(1);
+        }}>-</button>
+        <span>{pencount}</span>
       </div>
       <div className="items">
-        <span>Ink Pens:{inkpens} </span>
-        <button onClick={()=> handleInkPen(1)} className="circlularButton">+</button>
-        <button onClick={()=> handleInkPen(-1)} className="circlularButton">-</button>
-        <span>{inv.inkpens}</span>
+        <span>Ink Pens: </span>
+        <button className="circlularButton" onClick={() => {
+            inkpenSubHandle(1);
+        }}>+</button>
+        <button className="circlularButton" onClick={() => {
+            inkpenSubHandle(1);
+        }}>-</button>
+        <span>{inkpencount}</span>
       </div>
             {/*calculate total and show it*/}
-      total: {0}
+      total: {bookcount + pencount + notebookcount + inkpencount}
     </div>
   );
 };
