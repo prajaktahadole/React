f3687a2b1135544194d6c724201f43d8eda866ed
prajaktahadole <prajaktahadole@gmail.com>
2022-05-14 23:27:27 +0530

    update

--- 5 UseEffect/app/src/App.jsx
+++ 5 UseEffect/app/src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { Hello } from './component/sample'
+import { Hello, Example2} from './component/sample';
+
+ 
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
+  const [show, setShow ] = useState(true);
   return (
     <div className="App">
-     <Hello></Hello>
-       
+     
+     <div>
+     {show ? <Hello/>: null}
+       <button onClick={() =>{
+           setShow(!show);
+       }}>Toggle component</button>
+     </div>
+
+       <div>
+       <Example2></Example2>
+       </div>
+
+     
     </div>
   )
 }
--- 5 UseEffect/app/src/component/sample.jsx
+++ 5 UseEffect/app/src/component/sample.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 
 
 export const Hello = () =>{
-    const [show, setShow ] = useState(true);
-    return (
-       <div > {show ? <Hello/> : null}Hello
-       <button onClick={() =>{
-           setShow(!show);
-       }}>Toggle</button>
-       
-       </div>
+   
+    return ( 
+       <div > Hello    </div>
     )
 }    
+
+
+export const Example2 = () =>{
+
+    const [show, setShow] = useState(true);
+    console.log("Before");
+
+    useEffect(() =>{
+        console.log("Inside effect 1");
+    });
+
+    useEffect(() =>{
+        console.log("Inside effect 2");
+    });
+
+
+    console.log("After");
+
+    return (
+        "Hello World"
+    )
+}
