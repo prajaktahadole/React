1b789e764b21532f81c7b91bed367dee98727e76
prajaktahadole <prajaktahadole@gmail.com>
2022-05-19 23:48:53 +0530

    update

--- 5 UseEffect/app/src/component/sample.jsx
+++ 5 UseEffect/app/src/component/sample.jsx
@@ -4,28 +4,39 @@ import { useState ,useEffect} from "react";
 export const Hello = () =>{
    
     return ( 
-       <div > Hello    </div>
+       <div > Hello  </div>
+
+     
     )
 }    
 
 
 export const Example2 = () =>{
 
-    const [show, setShow] = useState(true);
+    const [count, setCount] = useState(0);
+    console.log(count);
     console.log("Before");
-
-    useEffect(() =>{
+                                                        /// it 1st print 1 and 2 , then it goes inside the useEffect 
+    // mounting phase                                                   // and print Inside effect 1,   Inside effect 2                                                   
+    useEffect(() =>{                                    // it means React will run  this useEffect code after the UI shown to the user                
         console.log("Inside effect 1");
-    });
+    } , []);
+    // [] which means no dependancies, in this case useEffect get called only once
 
     useEffect(() =>{
-        console.log("Inside effect 2");
-    });
+       // console.log("Inside effect 2");
+        document.title = `you have clicked ${count} times`
+    },[count]);
 
 
     console.log("After");
 
     return (
-        "Hello World"
+      <div>
+            <br></br>
+             <hr></hr>
+            <h1>Count : {count}</h1>
+            <button onClick={() => setCount(count+1)}>ADD</button>
+      </div>
     )
 }
