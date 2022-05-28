cc2a27f09dd544ee0382d46b993914790c386da0
prajaktahadole <prajaktahadole@gmail.com>
2022-05-28 23:59:16 +0530

    3rd done

--- Layout/src/components/ex3.jsx
+++ Layout/src/components/ex3.jsx
@@ -1,9 +1,22 @@
-import { Box , Heading} from "@chakra-ui/react"
+import { Box , Heading, Grid, GridItem} from "@chakra-ui/react"
 
 function Example3 (){
     return (
        <Box>
             <Heading>LAYOUT - 3</Heading>
+
+            <Grid
+           h='1000px'
+           templateRows='repeat(6, 1fr)'
+           templateColumns='repeat(6, 1fr)'
+           gap={4}>
+              <GridItem  rowSpan={[1, 2, 2]} colSpan={[6 , 3, 3]} bg='sandybrown' ></GridItem>
+              <GridItem  rowSpan={[1, 2 , 2]} colSpan={[6 , 3, 3]} bg='sandybrown' ></GridItem>
+              <GridItem  rowSpan={[2, 2, 2]} colSpan={[6, 6, 6]} bg='tomato'></GridItem>
+              <GridItem  rowSpan={[1, 1, 2]} colSpan={[6, 6 , 2]} bg='mediumslateblue'></GridItem>
+              <GridItem  rowSpan={[1, 1, 2]} colSpan={[6, 6 , 2]} bg='mediumslateblue'></GridItem>
+              <GridItem  rowSpan={[0, 0, 2]} colSpan={[6, 6 , 2]} bg='mediumslateblue'></GridItem>
+          </Grid>
 
 
        </Box>
